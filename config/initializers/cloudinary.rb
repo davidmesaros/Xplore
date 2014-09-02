dc251677337f3c977aa40858704d6642a170e641
davidmesaros <david.mesaros@hotmail.com>
2014-09-02 19:22:10 +1000

    testing

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'davidmesaros'
-  config.api_key = '814562171466317'
-  config.api_secret = 'ZifzuSM5DxevlnZzdEZPA1E0ogQ'
-  config.cdn_subdomain = true
-end
+# Cloudinary.config do |config|
+#   config.cloud_name = 'davidmesaros'
+#   config.api_key = '814562171466317'
+#   config.api_secret = 'ZifzuSM5DxevlnZzdEZPA1E0ogQ'
+#   config.cdn_subdomain = true
+# end
